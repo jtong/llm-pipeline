77bb4da337d4171aef63786b23076f24c595ce5a
jtong <tj19832@gmail.com>
2024-03-17 19:43:53 +0800

    add factory to index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const common_pipes = require("./common_pipes")
 const pipelines = require('./pipelines');
 const openai_client = require("./openai_client");
+const openai_client_factory = require("./openai_client/factory");
 
 module.exports = {
     common_pipes,
     pipelines,
     clients: {
-        openai_client
+        openai_client,
+        openai_client_factory
     }
 };
